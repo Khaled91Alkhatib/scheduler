87f97beab412d8226e83aed67e49f63ae331a572
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-15 12:54:58 -0400

    Implement function so that the tests pass depending on number of spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,15 +3,27 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  
+  const formatSpots = function() {
+    if(props.spots === 0) {
+      return "no spots remaining";
+    } else if(props.spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${props.spots} spots remaining` ;
+    }
+  }
+
   // Apply conditional styling depending on given props
   let dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   })
+
   return (
     <li className={dayClass} onClick = {() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
