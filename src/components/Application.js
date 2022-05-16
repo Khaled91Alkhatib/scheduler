7d13530249ec84b4a89571dfcf5cac585d9715cf
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-16 19:58:42 -0400

    Use the words value and onChange to mimic a standard HTML <select> list

--- src/components/Application.js
+++ src/components/Application.js
@@ -39,8 +39,8 @@ export default function Application(props) {
         <DayList
 
           days={days}
-          day={day}
-          setDay={setDay}
+          value={day}
+          onChange={setDay}
         />
       </nav>
       <img
