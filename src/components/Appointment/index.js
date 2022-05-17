3c35180773618c0b0af8e476c0ec20dee2709426
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-17 17:51:49 -0400

    Add several components and use coditional rendering to call them

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,16 @@
 import React from "react";
-import "components/Appointment/styles.scss"
+import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <header>
+        <Header time={props.time} />
+        {props.interview ? <Show /> : <Empty />}
+      </header>
+    </article>
   )
 }
