8e13ec6cd64c1033c8dcf524625b2ee47c347214
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-21 11:43:48 -0400

    Create a selector that returns an array of appointments for a given day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,28 @@
+export function getAppointmentsForDay(state, day) {
+
+  // Filter through state.days to get an object of every indiviual day that matches the parameter day
+  const foundDay = state.days.filter(filteredDay => filteredDay.name === day)[0];
+  console.log(foundDay);
+
+  // This handles the possibility of not having a given day
+  if (!foundDay) {
+    return [];
+  }
+
+  // First approach to return the array of appointments is by using for...of loop
+  const results = [];
+  for (const key of foundDay.appointments) {
+    console.log(state.appointments[key]);
+    if (state.appointments[key]) {
+      results.push(state.appointments[key]);
+    }
+  }
+  return results;
+
+  // Another approach is possible by using .map()
+  // return foundDay.appointments.map(id => {
+  //   if (state.appointments[id]) {
+  //     return state.appointments[id];
+  //   }
+  // });
+}
