82c3c0ee30dadb4f61e1547494c28c2dbfe634d2
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-21 12:30:45 -0400

    Combine state and have separate actions to update certain parts of the state

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,8 +46,15 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // Using spread operator to create a new object with all the existing keys of state
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
+  // Combining states
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+  });
 
   useEffect(() => {
     // Retrieve the days from the api
@@ -80,8 +87,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
