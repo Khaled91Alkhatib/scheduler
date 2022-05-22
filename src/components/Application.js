456bd4d008e44441d33a207a3577f347e36822a6
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-22 16:13:18 -0400

    Retrieve interviewer data by fetching it and transforming it

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   // Using spread operator to create a new object with all the existing keys of state
@@ -15,30 +15,36 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   useEffect(() => {
     const daysUrl = `http://localhost:8001/api/days`;
     const appointmentsUrl = 'http://localhost:8001/api/appointments';
+    const interviewersUrl = 'http://localhost:8001/api/interviewers';
 
     // Promise.all will make all requests before updating the state
     Promise.all([
       axios.get(daysUrl),
-      axios.get(appointmentsUrl)
+      axios.get(appointmentsUrl),
+      axios.get(interviewersUrl)
     ]).then((all) => {
       // console.log("all", all);
-      const newDaysState = all[0].data; 
+      const newDaysState = all[0].data;
       const newAppointmentsState = all[1].data;
+      const newInterviewersState = all[2].data;
+      // console.log("newinterviewers", newInterviewersState)
 
       setState(prev => ({
         ...prev,
         // day: newDaysState[0].name,
         days: newDaysState,
-        appointments: newAppointmentsState
+        appointments: newAppointmentsState,
+        interviewers: newInterviewersState
       }));
     });
-
+    // console.log("state.inetr", state.interviewers)
     // Retrieve the days from the api
     // axios.get(`http://localhost:8001/api/days`)
     //   .then(response => {
@@ -52,15 +58,18 @@ export default function Application(props) {
   if (state.days) {
     dailyAppointments = getAppointmentsForDay(state, state.day);
   }
+  // console.log("state", state);
 
   // console.log("state.day", state.day)
-  const newArray = dailyAppointments.map((appointment) => {
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    // console.log(dailyAppointments)
     return <Appointment
       key={appointment.id}
-      {...appointment} // This way spreads the object into the props definition intead of writing them one by one
-    // id={appointment.id}
-    // time={appointment.time}
-    // interview={appointment.interview}
+      // {...appointment} // This way spreads the object into the props definition intead of writing them one by one
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
     />;
   });
 
@@ -89,7 +98,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {newArray}
+        {schedule}
       </section>
     </main>
   );
