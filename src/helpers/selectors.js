0ae9004d49f8929480dfd88df1711790bf942aa8
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-27 14:59:51 -0400

    Implement getInterviewersForDay function by copying and modifying getAppointmentsForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -45,3 +45,32 @@ export function getInterview(state, interview) {
   }
   return interviewObject;
 }
+
+export function getInterviewersForDay(state, day) {
+
+  // Filter through state.days to get an object of every indiviual day that matches the parameter day
+  const foundDay = state.days.filter(filteredDay => filteredDay.name === day)[0];
+  // console.log(foundDay);
+
+  // This handles the possibility of not having a given day
+  if (!foundDay) {
+    return [];
+  }
+
+  // First approach to return the array of interviewers is by using for...of loop
+  const results = [];
+  for (const key of foundDay.interviewers) {
+    // console.log(state.appointments[key]);
+    if (state.interviewers[key]) {
+      results.push(state.interviewers[key]);
+    }
+  }
+  return results;
+
+  // Another approach is possible by using .map()
+  // return foundDay.appointments.map(id => {
+  //   if (state.appointments[id]) {
+  //     return state.appointments[id];
+  //   }
+  // });
+}
