65baf61be55d01d4a50f798698163459742afe33
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-27 12:46:59 -0400

    Add CREATE constant and make the onAdd function to transition correctly if mode is EMPTY

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -17,7 +18,7 @@ export default function Appointment(props) {
     <article className="appointment">
       <header>
         <Header time={props.time} />
-        {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
         {mode === SHOW && (
           <Show
             student={props.interview.student}
