64bd832d8725f37b1f90732b3c46dc2569b4ff82
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-27 12:42:21 -0400

    Import custom hook and implement a conditional using it to determine initial mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,28 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <header>
         <Header time={props.time} />
-        {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty />}
+        {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer.name}
+          />
+        )}
       </header>
     </article>
-  )
+  );
 }
