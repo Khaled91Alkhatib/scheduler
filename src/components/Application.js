cbdc0c69e6831bd68892801cd8b159ed5c4e19d4
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-30 11:55:03 -0400

    Implementing updated appointments by adding new data to state

--- src/components/Application.js
+++ src/components/Application.js
@@ -62,7 +62,23 @@ export default function Application(props) {
 
   function bookInterview(id, interview) {
     console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    setState({ ...state, appointments });
   }
+
+  // const [state, setState] = useState({
+  //   day: "Monday",
+  //   days: [],
+  //   appointments: {},
+  //   interviewers: {}
+  // });
 
   // console.log("state.day", state.day)
   const interviewers = getInterviewersForDay(state, state.day);
