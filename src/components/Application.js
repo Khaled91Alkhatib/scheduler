1881e22d1209c197a8df28f458b6bc9b5d9d831b
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-06-02 14:41:10 -0400

    Remove some of the logic part of the cood to a seperate hook and import it / Refactoring

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,58 +1,21 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  // Using spread operator to create a new object with all the existing keys of state
-  const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({ ...prev, days }));
+
+  // Keys returned from useApplicationData hook
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
+
   let dailyAppointments = [];
-
-  // Combining states
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  useEffect(() => {
-    const daysUrl = `http://localhost:8001/api/days`;
-    const appointmentsUrl = 'http://localhost:8001/api/appointments';
-    const interviewersUrl = 'http://localhost:8001/api/interviewers';
-
-    // Promise.all will make all requests before updating the state
-    Promise.all([
-      axios.get(daysUrl),
-      axios.get(appointmentsUrl),
-      axios.get(interviewersUrl)
-    ]).then((all) => {
-      // console.log("all", all);
-      const newDaysState = all[0].data;
-      const newAppointmentsState = all[1].data;
-      const newInterviewersState = all[2].data;
-      // console.log("newinterviewers", newInterviewersState)
-
-      setState(prev => ({
-        ...prev,
-        // day: newDaysState[0].name,
-        days: newDaysState,
-        appointments: newAppointmentsState,
-        interviewers: newInterviewersState
-      }));
-    });
-    // console.log("state.inetr", state.interviewers)
-    // Retrieve the days from the api
-    // axios.get(`http://localhost:8001/api/days`)
-    //   .then(response => {
-    // Setting the days state with the data from the response
-    // setDays(response.data);
-    //   console.log("response", response);
-    // });
-  }, []);
 
   // Pass the function only if state.days exists/is true
   if (state.days) {
@@ -60,37 +23,6 @@ export default function Application(props) {
   }
   console.log("state", state);
 
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    // The code below will make the new appointments persist after browser refresh
-    return axios
-      .put(`/api/appointments/${id}`, { interview })
-      .then(() => { setState({ ...state, appointments }); });
-
-  }
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios
-      .delete(`/api/appointments/${id}`)
-      .then(() => { setState({ ...state, appointments }); });
-  }
 
   // console.log("state.day", state.day)
   const interviewers = getInterviewersForDay(state, state.day);
@@ -109,7 +41,6 @@ export default function Application(props) {
       cancelInterview={cancelInterview}
     />;
   });
-
 
   // console.log("daily", dailyAppointments);
   // console.log("state", state)
