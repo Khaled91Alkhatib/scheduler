cd07d1bdbede35daaa1ed152066f1d1fbca8ecea
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-06-06 23:13:23 -0400

    Add conditional to check if student name or interviewer is empty and pass it in return

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from 'components/InterviewerList';
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewerId, setInterviewerId] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   // Functions used in <Button /> to handle onClick
   const reset = function () {
@@ -18,7 +19,20 @@ export default function Form(props) {
   };
 
   // This represents whet will the onSave prop in Form component will do
-  const saveData = function () {
+  // const saveData = function () {
+  //   props.onSave(student, interviewerId);
+  // };
+
+  const validate = function () {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewerId === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    setError("");
     props.onSave(student, interviewerId);
   };
 
@@ -35,6 +49,7 @@ export default function Form(props) {
             data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewerId}
@@ -44,7 +59,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button onClick={saveData} confirm>Save</Button>
+          <Button onClick={validate} confirm>Save</Button>
         </section>
       </section>
     </main>
